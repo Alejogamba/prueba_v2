a3f51078967e84d08ae2e9e1ba86627c58fa37ba
Alejogamba <gambajimenez@gmail.com>
2023-10-07 14:30:41 -0500

    first commit v2

---
+++ barcelona - copia.php
@@ -0,0 +1,21 @@
+<?php
+$equipo = "Real Madrid";
+
+switch ($equipo) {
+    case "Barcelona":
+        echo "El Barcelona es un equipo de fútbol de La Liga en España.";
+        break;
+    case "Real Madrid":
+        echo "El Real Madrid es otro equipo destacado de La Liga.";
+        break;
+    case "Manchester United":
+        echo "El Manchester United es un equipo de la Premier League en Inglaterra.";
+        break;
+    case "Bayern Munich":
+        echo "El Bayern Munich es uno de los equipos más exitosos de la Bundesliga en Alemania.";
+        break;
+    default:
+        echo "No tengo información sobre ese equipo de fútbol.";
+        break;
+}
+?>
